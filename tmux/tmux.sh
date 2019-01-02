82286498508253623d59527d9382c2d353248b97
Karim Echchennouf <k.echchennouf@gmail.com>
2019-01-02 22:12:07 +0100

    inial commit

---
+++ tmux/tmux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+SESSION=main
+tmux="tmux -2 -f tmux/tmux.conf"
+
+# if the session is already running, just attach to it.
+$tmux has-session -t $SESSION
+if [ $? -eq 0 ]; then
+       echo "Session $SESSION already exists. Attaching."
+       sleep 1
+       $tmux attach -t $SESSION
+       exit 0;
+fi
+                                 
+# create a new session, named $SESSION, and detach from it
+$tmux new-session -d -s $SESSION
+$tmux new-window    -t $SESSION:1 
+$tmux split-window  -h -t $SESSION:1
+$tmux split-window  -v -t $SESSION:1
+$tmux split-window  -h -t $SESSION:1
+$tmux split-window  -v -t $SESSION:1
+$tmux new-window    -t $SESSION:2
+$tmux split-window  -h -t $SESSION:2
+$tmux split-window  -v -t $SESSION:2
+$tmux attach -t $SESSION
