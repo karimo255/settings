935957dcf1c62d2485b52cf90fb35a3fe8c8bd8e
Karim Echchennouf <k.echchennouf@gmail.com>
2019-01-11 00:43:14 +0100

    add z and update vim config

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "copy pomodoro binary to /usr/local/bin/"
-
 cp tmux/bin/pomodoro /usr/local/bin/
 
 apt-get install cmus
+
+echo "Install vim-gtk to copy from/to system clipboard"
+apt-get install vim-gtk
