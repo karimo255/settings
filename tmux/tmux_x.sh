11e452f604f162d2ca090d7131eddebad3709942
Karim Echchennouf <k.echchennouf@gmail.com>
2019-08-14 22:21:32 +0200

    feat: now nourpos project path can be set as variable

--- tmux/tmux_x.sh
+++ tmux/tmux_x.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+
 SESSION=main
 tmux="tmux -2 -f /home/karim/settings/tmux/tmux.conf"
 windows_count=`tmux list-windows | wc -l`
+nourpos_path="/home/karim/nourpos"
+
 # create a new session, named $SESSION, and detach from it
+
 $tmux new-window    -t $SESSION:$(($windows_count + 1))
-$tmux send-keys "cd /home/karim/nourpos/packages/wss && npm start" C-m
+$tmux send-keys "cd $nourpos_path/packages/wss && npm start" C-m
 
 $tmux split-window  -h -p 84 -t $SESSION:$(($windows_count + 1))
-$tmux send-keys "cd /home/karim/nourpos/packages/updater && npm start" C-m
+$tmux send-keys "cd $nourpos_path/packages/updater && npm start" C-m
 
 $tmux split-window  -h -p 80 -t $SESSION:$(($windows_count + 1))
-$tmux send-keys "cd /home/karim/nourpos/packages/auth && npm start" C-m
+$tmux send-keys "cd $nourpos_path/packages/auth && npm start" C-m
 
 $tmux split-window  -v -t $SESSION:$(($windows_count + 1))
-$tmux send-keys "cd /home/karim/nourpos/packages/api-gateway && npm start" C-m
+$tmux send-keys "cd $nourpos_path/packages/apiGateway && npm start" C-m
 
 $tmux split-window  -h -t $SESSION:$(($windows_count + 1))
-$tmux send-keys "cd /home/karim/nourpos/packages/rest-api && npm start" C-m
+$tmux send-keys "cd $nourpos_path/packages/restApi && npm start" C-m
